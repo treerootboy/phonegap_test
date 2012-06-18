a9ecb640531483a086656b768361a312abd4f182
treerootboy <treerootboy@gmail.com>
2012-06-18 17:51:10 +0800

    Sencha Touch Test 2

--- app.js
+++ app.js
@@ -1,49 +1,103 @@
 Ext.application({
-    models: ["User"],
 
     name: '8891',
 
-    requires: [
-        'Ext.MessageBox'
-    ],
-
-    views: ['Main'],
-
-    icon: {
-        '57': 'resources/icons/Icon.png',
-        '72': 'resources/icons/Icon~ipad.png',
-        '114': 'resources/icons/Icon@2x.png',
-        '144': 'resources/icons/Icon~ipad@2x.png'
-    },
-
-    isIconPrecomposed: true,
-
-    startupImage: {
-        '320x460': 'resources/startup/320x460.jpg',
-        '640x920': 'resources/startup/640x920.png',
-        '768x1004': 'resources/startup/768x1004.png',
-        '748x1024': 'resources/startup/748x1024.png',
-        '1536x2008': 'resources/startup/1536x2008.png',
-        '1496x2048': 'resources/startup/1496x2048.png'
-    },
-
     launch: function() {
-        // Destroy the #appLoadingIndicator element
-        Ext.fly('appLoadingIndicator').destroy();
+        Ext.create('Ext.tab.Panel', {
+            fullscreen: true,
+            tabBarPosition: 'bottom',
 
-        // Initialize the main view
-        Ext.Viewport.add(Ext.create('8891.view.Main'));
-    },
+            items: [
+                {
+                    title: 'Welcome',
+                    iconCls: 'home',
+                    cls: 'home',
 
-    onUpdated: function() {
-        Ext.Msg.confirm(
-            "Application Update",
-            "This application has just successfully been updated to the latest version. Reload now?",
-            function(buttonId) {
-                if (buttonId === 'yes') {
-                    window.location.reload();
+                    html: [
+                        '<img src="http://img.8891.com.tw/common/logo.gif"/>',
+                        '<h1>Welcome to 8891</h1>',
+                        '<p>This is a 8891 app test!</p>',
+                        '<p>Welcome and enjoey it!</p>'
+                    ].join("")
+                },
+                {
+                    xtype: 'nestedlist',
+                    title: 'Blog Feed',
+                    iconCls: 'star',
+                    displayField: 'title',
+
+                    store: {
+                        type: 'tree',
+
+                        fields: [
+                            'title', 'link', 'author', 'contentSnippet', 'content',
+                            {name: 'leaf', defaultValue: true}
+                        ],
+
+                        root: {
+                            leaf: false
+                        },
+
+                        proxy: {
+                            type: 'jsonp',
+                            url: 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://blog.treerootboy.info/?feed=rss2',
+                            reader: {
+                                type: 'json',
+                                rootProperty: 'responseData.feed.entries'
+                            }
+                        }
+                    },
+
+                    detailCard: {
+                        xtype: 'panel',
+                        scrollable: true,
+                        styleHtmlContent: true
+                    },
+
+                    listeners: {
+                        itemtap: function(nestedList, list, index, element, post) {
+                            this.getDetailCard().setHtml(post.get('content'));
+                        }
+                    }
+                },
+                {
+                    title: 'Contact',
+                    iconCls: 'user',
+                    xtype: 'formpanel',
+                    url: 'contact.php',
+                    layout: 'vbox',
+
+                    items: [
+                        {
+                            xtype: 'fieldset',
+                            title: 'Contact Us',
+                            instructions: '(email address is optional)',
+                            items: [
+                                {
+                                    xtype: 'textfield',
+                                    label: 'Name'
+                                },
+                                {
+                                    xtype: 'emailfield',
+                                    label: 'Email'
+                                },
+                                {
+                                    xtype: 'textareafield',
+                                    label: 'Message'
+                                }
+                            ]
+                        },
+                        {
+                            xtype: 'button',
+                            text: 'Send',
+                            ui: 'confirm',
+                            handler: function(){
+                                this.up('formpanel').submit();
+                            }
+                        }
+                    ]
                 }
-            }
-        );
+            ]
+        });
     }
 });
